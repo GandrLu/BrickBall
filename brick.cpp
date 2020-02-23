8380353f379b608960d6b605fdb786f2260d726e
GandrLu <acidyumyum@web.de>
2020-02-23 12:00:44 +0100

    Add brick logics

--- brick.cpp
+++ brick.cpp
@@ -1,7 +1,50 @@
 #include "brick.h"
 #include <QGraphicsScene>
 
-Brick::Brick()
+Brick::Brick(BrickType _type, int _BrickWidth, int _BrickHeight)
+	: m_Type(_type)
 {
+	switch (m_Type)
+	{
+	case red:
+		setPixmap(QPixmap(":/images/resources/images/brick_red.png").scaled(_BrickWidth, _BrickHeight));
+		m_PointValue = 50;
+		m_LifePoints = 3;
+		break;
+	case blue:
+		setPixmap(QPixmap(":/images/resources/images/brick_blue.png").scaled(_BrickWidth, _BrickHeight));
+		m_PointValue = 10;
+		m_LifePoints = 1;
+		break;
+	case green:
+		setPixmap(QPixmap(":/images/resources/images/brick_green.png").scaled(_BrickWidth, _BrickHeight));
+		m_PointValue = 20;
+		m_LifePoints = 1;
+		break;
+	case brown:
+		setPixmap(QPixmap(":/images/resources/images/brick_brown.png").scaled(_BrickWidth, _BrickHeight));
+		m_PointValue = 30;
+		m_LifePoints = 2;
+		break;
+	default:
+		setPixmap(QPixmap(":/images/resources/images/brick_blue.png").scaled(_BrickWidth, _BrickHeight));
+		m_PointValue = 10;
+		m_LifePoints = 1;
+		break;
+	}
+}
 
+void Brick::m_ReduceLifePoints(int _Value)
+{
+	this->m_LifePoints -= _Value;
+}
+
+int Brick::m_GetPointValue()
+{
+	return this->m_PointValue;
+}
+
+int Brick::m_GetLifePoints()
+{
+	return this->m_LifePoints;
 }
--- brick.h
+++ brick.h
@@ -2,11 +2,26 @@
 #define BRICK_H
 
 #include <QGraphicsRectItem>
+enum BrickType
+{
+    red,
+    blue,
+    green,
+    brown
+};
 
 class Brick : public QGraphicsPixmapItem
 {
 public:
-    Brick();
+    Brick(BrickType _type, int _BrickWidth, int _BrickHeight);
+public:
+    void m_ReduceLifePoints(int _Value = 1);
+    int m_GetPointValue();
+    int m_GetLifePoints();
+private:
+    BrickType m_Type;
+    int m_PointValue;
+    int m_LifePoints;
 };
 
 #endif // BRICK_H
